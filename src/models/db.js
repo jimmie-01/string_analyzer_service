8641b815595ad1de2f4accf0308f9645be932fc5
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-10-22 23:13:28 +0100

    DB migration update

--- src/models/db.js
+++ src/models/db.js
@@ -1,27 +1,38 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-});
+async function migrate() {
+	const pool = new Pool({
+		connectionString: process.env.DATABASE_URL,
+		ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+	});
+	try
+	{
+		// Create table if non exists
+		const createTableQuery = `
+		CREATE TABLE IF NOT EXISTS analyzed_strings(
+		id VARCHAR(64) PRIMARY KEY,
+		value TEXT NOT NULL UNIQUE,
+		length INTEGER NOT NULL,
+		is_palindrome BOOLEAN NOT NULL,
+		unique_characters INTEGER NOT NULL,
+		word_count INTEGER NOT NULL,
+		character_frequency_map JSONB NOT NULL,
+		created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+		);
+		`;
+		await pool.query(createTableQuery);
+		console.log('Database migration comleted successfully')
+	} catch (error) {
+		console.log('Migration Failed:', error);
+		process.exit(1);
+	} finally {
+		await pool.end();
+	}
+}
 
-// Create table if non exists
-const createTableQuery = `
-CREATE TABLE IF NOT EXISTS analyzed_strings(
-id VARCHAR(64) PRIMARY KEY,
-value TEXT NOT NULL UNIQUE,
-length INTEGER NOT NULL,
-is_palindrome BOOLEAN NOT NULL,
-unique_characters INTEGER NOT NULL,
-word_count INTEGER NOT NULL,
-character_frequency_map JSONB NOT NULL,
-created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-`;
-
-pool.query(reateTableQuery)
-	.then(() => console.log('Database table ready'))
-	.catch(err => console.error('Database setup error:', err));
-
-module.exports = pool;
+// Run If called directly
+if (require.main === module) {
+	migrate();
+}
+module.exports = migrate;
