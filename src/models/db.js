0817d5e7347998e82127db8b8aee56f947453fc3
Fakile Razaq <razaq_jimmie@yahoo.com>
2025-10-22 23:27:47 +0100

    Database migration function

--- src/models/db.js
+++ src/models/db.js
@@ -1,38 +1,16 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-async function migrate() {
-	const pool = new Pool({
-		connectionString: process.env.DATABASE_URL,
-		ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-	});
-	try
-	{
-		// Create table if non exists
-		const createTableQuery = `
-		CREATE TABLE IF NOT EXISTS analyzed_strings(
-		id VARCHAR(64) PRIMARY KEY,
-		value TEXT NOT NULL UNIQUE,
-		length INTEGER NOT NULL,
-		is_palindrome BOOLEAN NOT NULL,
-		unique_characters INTEGER NOT NULL,
-		word_count INTEGER NOT NULL,
-		character_frequency_map JSONB NOT NULL,
-		created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-		);
-		`;
-		await pool.query(createTableQuery);
-		console.log('Database migration comleted successfully')
-	} catch (error) {
-		console.log('Migration Failed:', error);
-		process.exit(1);
-	} finally {
-		await pool.end();
-	}
-}
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+});
 
-// Run If called directly
-if (require.main === module) {
-	migrate();
-}
-module.exports = migrate;
+pool.query('SELECT NOW()')
+	.then(() => console.log('Connected to PostgreSQL on Railway'))
+	.catch(err => {
+		comnsole.error('Database connection failed:', err);
+		process.exit(1);
+	});
+
+module.exports = pool;
